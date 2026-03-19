b0c318042b1726595457793f45851949745cb9bf
Rubossie-spec <Rubossie@icloud.com>
2026-03-19 11:31:40 +0100

    Update game.js

--- game.js
+++ game.js
@@ -3,16 +3,15 @@ type: Phaser.AUTO,
 width: 800,
 height: 400,
 parent: "game",
-backgroundColor: "#000000",
+backgroundColor: "#111",
 physics: {
 default: "arcade",
 arcade: {
-gravity: { y: 800 },
+gravity: { y: 900 },
 debug: false
 }
 },
 scene: {
-preload: preload,
 create: create,
 update: update
 }
@@ -22,43 +21,46 @@ const game = new Phaser.Game(config)
 
 let player
 let obstacles
+let cursors
 let score = 0
 let scoreText
 
-function preload(){}
-
 function create(){
 
-scoreText = this.add.text(10,10,"Score: 0",{fontSize:"20px",fill:"#ffffff"})
+scoreText = this.add.text(20,20,"Score: 0",{fontSize:"20px",fill:"#fff"})
 
-player = this.physics.add.rectangle(100,300,40,40,0x00ff00)
+player = this.add.rectangle(100,300,40,40,0x00ff00)
 
-player.setCollideWorldBounds(true)
+this.physics.add.existing(player)
+
+player.body.setCollideWorldBounds(true)
 
 obstacles = this.physics.add.group()
 
+cursors = this.input.keyboard.createCursorKeys()
+
 this.time.addEvent({
-delay:2000,
+delay:1500,
 callback: spawnObstacle,
 callbackScope:this,
 loop:true
 })
 
-this.input.keyboard.on("keydown-SPACE", ()=>{
-
-if(player.body.touching.down){
-player.setVelocityY(-450)
-}
-
-})
+this.physics.add.overlap(player, obstacles, hitObstacle, null, this)
 
 }
 
 function update(){
 
+if(cursors.up.isDown && player.body.touching.down){
+player.body.setVelocityY(-450)
+}
+
 obstacles.children.iterate(function(obstacle){
 
 if(obstacle){
+
+obstacle.x -= 5
 
 if(obstacle.x < -50){
 
@@ -66,7 +68,7 @@ obstacle.destroy()
 
 score++
 
-scoreText.setText("Score: "+score)
+scoreText.setText("Score: " + score)
 
 }
 
@@ -78,12 +80,18 @@ scoreText.setText("Score: "+score)
 
 function spawnObstacle(){
 
-let obstacle = this.physics.add.rectangle(800,320,40,40,0xff0000)
+let obstacle = this.add.rectangle(800,320,40,40,0xff0000)
 
-obstacle.setVelocityX(-250)
-
-obstacle.setImmovable(true)
+this.physics.add.existing(obstacle)
 
 obstacles.add(obstacle)
+
+}
+
+function hitObstacle(){
+
+alert("Game Over! Score: " + score)
+
+location.reload()
 
 }
